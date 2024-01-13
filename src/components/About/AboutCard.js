e209f7707711e4b6a90da5698e3c980aa10a5758
suleiman yusuf <108791588+hetrox8@users.noreply.github.com>
2024-01-13 09:06:54 +0300

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -9,7 +9,7 @@ function AboutCard() {
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple">Suleiman Gacheru </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            from <span className="purple"> Mombasa, Kenya.</span>
             <br />
             I am Full-stack Developer working on Open Source Projects, Freelancing and Working on Personal Projects.
             <br />
